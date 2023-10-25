67e40418aa006c43706db53afd9c906477b905e4
Lalithya <lalithyanawanjana@gmail.com>
2023-10-25 20:11:43 +0530

    Create delete user api

--- server.js
+++ server.js
@@ -12,21 +12,7 @@ const app = express();
 require("dotenv/config");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
-
-
-/** ------------------------------------------- Create --------------------------------------*/
-
-//create a user
-app.post("/user/save", async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    res.status(200).json(user);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
+app.use(express.urlencoded({ extended: false }));
 
 //create a cruise
 app.post("/cruise/save", async (req, res) => {
@@ -131,14 +117,33 @@ app.get("/user/:id", async (req, res) => {
 //update a user by id
 app.put("/user/:id", async (req, res) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const user = await User.findByIdAndUpdate(id, req.body);
-    if(!user) {
-      return res.status(404).json({message: 'Can not find any user by id ' + id});
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "Can not find any user by id " + id });
     }
 
     const updatedUser = await User.findById(id);
     res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//delete a user
+app.delete("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByIdAndDelete(id);
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "Can not find any user by id " + id });
+    }
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
