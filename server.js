a0be47bf04385f1de58971b260eb668e6d7f36db
Lalithya <lalithyanawanjana@gmail.com>
2023-10-25 20:19:28 +0530

    Fixed

--- server.js
+++ server.js
@@ -112,6 +112,12 @@ app.get("/user/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const user = await User.findById(id);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "Can not find any user by id " + id });
+    }
+
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ message: error.message });
