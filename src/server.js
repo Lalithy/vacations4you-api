e1920576063459b7c962b7d96ac89f329364e2f7
Lali <lalithyanawanjana@gmail.com>
2023-11-07 19:41:07 +0530

    Change model of cruise

--- src/server.js
+++ src/server.js
@@ -22,7 +22,8 @@ var corsOptions = {
   optionsSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
