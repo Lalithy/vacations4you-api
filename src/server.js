0174fbe2eb858cd1f4d95e696929643e8b78b2d9
kowshi16 <112153230+kowshi16@users.noreply.github.com>
2023-11-08 15:09:31 +0530

    Changes in server.js

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ var corsOptions = {
   optionsSuccessStatus: 200,
 };
 
-//app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -37,20 +37,14 @@ app.use("/api/package/booking", packageBookingRoute);
 
 //db connection
 mongoose.set("strictQuery", false);
-// mongoose
-//   .connect(DB_URL)
-//   .then(() => console.log("Connected!"))
-//   .catch((error) => {
-//     console.log(error);
-//   });
-
-  mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
   })
   .catch(error => {
     console.error('Error connecting to MongoDB:', error);
   });
+
 
 app.listen(PORT, () => {
   console.log("Vacation4You API is running on port " + PORT);
