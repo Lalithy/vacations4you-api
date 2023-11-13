367dc7496d712a6b4740eec6bc9ff8ddaa21bcc3
Lali <lalithyanawanjana@gmail.com>
2023-11-13 09:42:41 +0530

    Upload from csv
     activity data & package data

--- src/api/controllers/csvUploadController.js
+++ src/api/controllers/csvUploadController.js
@@ -1,4 +1,6 @@
 const UploadCruise = require("../models/cruiseModel");
+const UploadActivity = require("../models/activityModel");
+const UploadPackage = require("../models/packageModel");
 const asyncHandler = require("express-async-handler");
 const csvtojson = require("csvtojson");
 
@@ -9,7 +11,37 @@ const uploadCruise = asyncHandler(async (req, res) => {
       req.file.buffer.toString("utf8")
     );
     await UploadCruise.insertMany(jsonArray);
-    res.status(200).json({ message: "CSV data successfully uploaded" });
+    res.status(200).json({ message: "CSV cruise data successfully uploaded" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Upload from csv activity data
+const uploadActivity = asyncHandler(async (req, res) => {
+  try {
+    const jsonArray = await csvtojson().fromString(
+      req.file.buffer.toString("utf8")
+    );
+    await UploadActivity.insertMany(jsonArray);
+    res
+      .status(200)
+      .json({ message: "CSV activity data successfully uploaded" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Upload from csv package data
+const uploadPackage = asyncHandler(async (req, res) => {
+  try {
+    const jsonArray = await csvtojson().fromString(
+      req.file.buffer.toString("utf8")
+    );
+    await UploadPackage.insertMany(jsonArray);
+    res.status(200).json({ message: "CSV package data successfully uploaded" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -18,4 +50,6 @@ const uploadCruise = asyncHandler(async (req, res) => {
 
 module.exports = {
   uploadCruise,
+  uploadActivity,
+  uploadPackage,
 };
--- src/api/routes/csvUploadRoute.js
+++ src/api/routes/csvUploadRoute.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { uploadCruise } = require("../controllers/csvUploadController");
+const {
+  uploadCruise,
+  uploadActivity,
+  uploadPackage,
+} = require("../controllers/csvUploadController");
 const multer = require("multer");
 
 const router = express.Router();
@@ -8,5 +12,7 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 router.post("/cruise", upload.single("csvFile"), uploadCruise);
+router.post("/activity", upload.single("csvFile"), uploadActivity);
+router.post("/package", upload.single("csvFile"), uploadPackage);
 
 module.exports = router;
